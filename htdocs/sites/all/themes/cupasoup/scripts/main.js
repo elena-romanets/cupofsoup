b1b65022bc8f7a1b4114a477f1bb2ff1d6617c4b
Tony Pashchenko <tony.pashchenko@gmail.com>
2016-05-17 03:00:43 +0300

    assortiment page updates

--- htdocs/sites/all/themes/cupasoup/scripts/main.js
+++ htdocs/sites/all/themes/cupasoup/scripts/main.js
@@ -2,6 +2,8 @@
   $('document').ready(function () {
 
     var $formPlaceholder = $('.form-placeholder');
+    var $table;
+    var $tableLastRow;
 
     $formPlaceholder.each(function () {
       var $el = $(this);
@@ -15,34 +17,95 @@
         var val = Number($input.val()) || 0;
 
         if (val > 0) {
-          $input.val(val - 1);
-          $input.change();
+          $input
+            .val(val - 1)
+            .change();
         }
       });
 
       $el.delegate('.plus', 'click', function () {
         var val = Number($input.val()) || 0;
 
-        $input.val(val + 1);
-        $input.change();
+        $input
+          .val(val + 1)
+          .change();
       });
     });
 
-
     var $allInputs = $formPlaceholder.find('input');
     $allInputs.change(function () {
+      var $el = $(this);
+      var id = $el.parent().data('id');
+
+      $('[data-id="' + id + '"] input').val($el.val());
 
       var totalPrice = 0;
+      var prices = {};
+      var items = {};
 
       $allInputs.each(function () {
         var $el = $(this);
+        var paneTitle = $el.parents('.assortiment-block').find('h2.pane-title').text();
+
+        if (paneTitle === 'POPULAIR') {
+          return;
+        }
+
         var val = Number($el.val()) || 0;
-        var price = $el.parents('tr').find('.product-assortiment-price-value').data('price')
+        var price = $el.parents('tr').find('.product-assortiment-price-value').data('price');
+        var productTitle = $el.parents('tr').find('.pane-title.sm-title').text();
+
+        if (!prices[paneTitle]) {
+          prices[paneTitle] = { total: 0 };
+        }
+        if (!items[paneTitle]) {
+           items[paneTitle] = { total: 0 };
+        }
+        if (!prices[paneTitle][productTitle]) {
+          prices[paneTitle][productTitle] = 0;
+        }
+        if (!items[paneTitle][productTitle]) {
+          items[paneTitle][productTitle] = 0;
+        }
+
+        prices[paneTitle].total += val * price;
+        items[paneTitle].total += val;
+        prices[paneTitle][productTitle] += val * price;
+        items[paneTitle][productTitle] += val;
         totalPrice += val * price;
       });
 
+      if (!$tableLastRow) {
+        $tableLastRow = $('.assortiment-total-price table tbody tr');
+        $table = $('.assortiment-total-price table tbody');
+      }
 
-      alert(totalPrice);
+      $tableLastRow.detach();
+      $table
+        .empty()
+        .append($tableLastRow);
+
+      var $totalPrice = $('.assortiment-total-price .total-wrapper span');
+
+
+      for (var i in prices) {
+        if (prices.hasOwnProperty(i) && prices[i].total) {
+
+          var $tr = $('<tr>')
+            .append(
+              $('<td>').text(i),
+              $('<td>').text(items[i].total + 'x'),
+              $('<td>').text(Math.round(prices[i].total * 100) / 100)
+            );
+
+          $tableLastRow.before($tr);
+        }
+      }
+
+      $totalPrice.text(Math.round(totalPrice * 100) / 100);
+      //console.log('totalPrice:', totalPrice);
+      //console.log('prices:', prices);
+      //console.log('items:', items);
     });
   });
 
