997fa9ad67364afc323ed3debadd6b56ad585655
Elena Romanets <elena@ebuilders.nl>
2016-06-01 10:25:46 +0300

    Changes in form sending

--- htdocs/sites/all/themes/cupasoup/scripts/main.js
+++ htdocs/sites/all/themes/cupasoup/scripts/main.js
@@ -13,33 +13,6 @@
       loop:true
     });
 
-    $('.field-name-field-title').each(function() {
-
-      var $inp = $('<input type="radio" name="product-option"/>');
-      $inp.val($(this).parent().find('.field-name-field-value .field-item').text());
-      $(this).prepend($inp)
-    });
-
-
-    var $assortimentLinks = $('.assortiment-menu .assortiment-link');
-    var $assortimentPanes = $('.assortiment-row');
-    $assortimentLinks.first().addClass('active');
-
-    $assortimentLinks.click(function () {
-      $assortimentLinks.removeClass('active');
-
-      var self = this;
-      var $el = $(this);
-      $el.addClass('active');
-
-      $assortimentLinks.each(function (index) {
-        if (self == this) {
-          $assortimentPanes.hide();
-          $assortimentPanes.eq(index).show()
-        }
-      })
-    });
-
 
     var $formPlaceholder = $('.form-placeholder');
     var $table;
@@ -76,139 +49,60 @@
     });
 
     var $allInputs = $formPlaceholder.find('input');
-    $allInputs.change(function () {
+    $allInputs.change(updateTotal);
+
+
+    $('.field-name-field-title .field-item').each(function(index) {
       var $el = $(this);
-      var id = $el.parent().data('id');
+      var id = 'radio-' + index;
+      var $inp = $('<input type="radio" name="product-option"/>');
+      $inp.val($el.text());//$(this).parents('.field-item').find('.field-name-field-value .field-item').text());
+      $inp.attr('id', id);
+      $inp.change(updateTotal);
 
-      $('[data-id="' + id + '"] input').val($el.val());
-
-      var totalPrice = 0;
-      var prices = {};
-      var items = {};
-
-      $allInputs.each(function () {
-        var $el = $(this);
-        var productTitle = $el.parents('.assortiment-block').find('h2.pane-title').text();
-
-        if (productTitle === 'POPULAIR') {
-          return;
-        }
-
-        var val = Number($el.val()) || 0;
-        var price = $el.parents('tr').find('.product-assortiment-price-value').data('price');
-        var paneTitle = $el.parents('.assortiment-row').find('.item-title').attr('data-title');
-
-        paneTitle += ' ' + productTitle;
-
-        if (!prices[paneTitle]) {
-          prices[paneTitle] = { total: 0 };
-        }
-        if (!items[paneTitle]) {
-           items[paneTitle] = { total: 0 };
-        }
-        if (!prices[paneTitle][productTitle]) {
-          prices[paneTitle][productTitle] = 0;
-        }
-        if (!items[paneTitle][productTitle]) {
-          items[paneTitle][productTitle] = 0;
-        }
-
-        prices[paneTitle].total += val * price;
-        items[paneTitle].total += val;
-        prices[paneTitle][productTitle] += val * price;
-        items[paneTitle][productTitle] += val;
-        totalPrice += val * price;
-      });
-
-      if (!$tableLastRow) {
-        $tableLastRow = $('.assortiment-total-price table tbody tr');
-        $table = $('.assortiment-total-price table tbody');
-      }
-
-      $tableLastRow.detach();
-      $table
+      var $label = $('<label for="' + id + '"/>');
+      $label.text($el.text());
+      $el
         .empty()
-        .append($tableLastRow);
+        .append($inp)
+        .append($label);
+    });
 
-      var $totalPrice = $('.assortiment-total-price .total-wrapper span');
-      var full = false;
+    var $assortimentLinks = $('.assortiment-menu .assortiment-link');
+    var $assortimentPanes = $('.assortiment-row');
+    $assortimentLinks.first().addClass('active');
 
-      for (var i in prices) {
-        if (prices.hasOwnProperty(i) && prices[i].total) {
-          full = true;
-          var $tr = $('<tr>')
-            .append(
-              $('<td>').text(i),
-              $('<td>').text(items[i].total + 'x'),
-              $('<td>').text(Math.round(prices[i].total * 100) / 100)
-            );
+    $assortimentLinks.click(function () {
+      $assortimentLinks.removeClass('active');
 
-          $tableLastRow.before($tr);
+      var self = this;
+      var $el = $(this);
+      $el.addClass('active');
+
+      $assortimentLinks.each(function (index) {
+        if (self == this) {
+          $assortimentPanes.hide();
+          $assortimentPanes.eq(index).show()
         }
-      }
-
-      if (full) {
-        $btnActive.addClass('active');
-        $res.show();
-        $emptyRes.hide();
-      }
-      else {
-        $btnActive.removeClass('active');
-        $res.hide();
-        $emptyRes.show();
-      }
-
-      $totalPrice.text(Math.round(totalPrice * 100) / 100);
-      //console.log('totalPrice:', totalPrice);
-      //console.log('prices:', prices);
-      //console.log('items:', items);
+      })
     });
 
+    // Address delivery is chosen
+    var $delivery = $('.delivery');
+    var $deliveryInput = $delivery.find('input');
+    $('.address-approve label').bind('click', function(){
 
+      $delivery.slideToggle('fast');
 
-    //  Form fields
-
-    $('.form-type-radio, .product-order-radio-btns .field-name-field-title').prepend('<span class="radio-button"></span>');
-
-    $('.form-type-radio input, .product-order-radio-btns .field-name-field-title input').each(function() {
-      if ($(this).is(':checked')) {
-        $('.form-type-radio .radio-button').removeClass('active');
-        $(this).parent('div').children('.radio-button').addClass('active');
+      if ($deliveryInput.attr('required')) {
+        $deliveryInput.removeAttr('required');
+      } else {
+        $deliveryInput.attr('required', 'required');
       }
     });
+    $('.delivery').hide();
 
-    $('.form-type-radio input, .product-order-radio-btns .field-name-field-title input').bind('change',function() {
-      if ($(this).is(':checked')) {
-        $('.form-type-radio .radio-button').removeClass('active');
-        $(this).parent('div').children('.radio-button').addClass('active');
-      }
-    });
-
-    $('.form-type-radio .radio-button, .product-order-radio-btns .field-name-field-title .radio-button').bind('click',function() {
-      $('.form-type-radio input').attr('checked','');
-      $('.product-order-radio-btns .field-name-field-title input').attr('checked','');
-      $(this).parent('div').children('input').attr('checked','checked');
-
-      $('.form-type-radio .radio-button').removeClass('active');
-      $('.product-order-radio-btns .field-name-field-title .radio-button').removeClass('active');
-      $(this).parent('div').children('.radio-button').addClass('active');
-    });
-
-    // Checkbox
-    $('.form-type-checkbox').prepend('<span class="checkbox-button"></span>');
-
-    $('.form-type-checkbox .checkbox-button').bind('click',function() {
-      if ($(this).parent('div').children('input').is(':checked')) {
-          $('.form-type-checkbox .checkbox-button').removeClass('active');
-          $(this).parent('div').children('input').attr('checked','');
-      }
-      else {
-        $(this).parent('div').children('.checkbox-button').addClass('active');
-        $(this).parent('div').children('input').attr('checked','checked');
-      }
-
-    });
-
+    // Mobile menuIcon
     $('.mob-menu .block-content').append('<span class="menuIcon"><span></span></span>');
 
     // init menu
@@ -229,8 +123,130 @@
       $(this).parent('div').toggleClass('show');
       $('.body-overlay').removeClass('show');
     });
+
+
+    var val = localStorage.getItem('items');
+    val = JSON.parse(val).join("\n");
+    $('.webform-component--items input[name="submitted[items]"]').val(val);
+
+
+    function updateTotal() {
+      var $el = $(this);
+      var id = $el.parent().data('id');
+
+      $('[data-id="' + id + '"] input').val($el.val());
+
+      var totalPrice = 0;
+      var prices = {};
+      var items = {};
+
+      $allInputs.each(function () {
+        var $el = $(this);
+        var categoryTitle = $el.parents('.assortiment-block').find('h2.pane-title').text();
+
+        if (categoryTitle === 'POPULAIR') {
+          return;
+        }
+
+        var $header = $('.header-details');
+        var classes = ['carrousel', 'mini', 'selectieboxen', 'tabletop', 'standalones'];
+        var mainTitle = '';
+
+        for (var i in classes) {
+          if ($header.hasClass(classes[i])) {
+            mainTitle = classes[i].toUpperCase();
+          }
+        }
+
+        var val = Number($el.val()) || 0;
+        var price = $el.parents('tr').find('.product-assortiment-price-value').data('price');
+        var paneTitleOrig = $el.parents('.assortiment-row').find('.item-title').attr('data-title') || mainTitle;
+        var productTitle = $el.parents('tr').find('.pane-title.sm-title').text();
+
+        var paneTitle = [ paneTitleOrig, categoryTitle ].join(' ');
+
+        if (!prices[paneTitle]) {
+          prices[paneTitle] = { total: 0 };
+        }
+        if (!items[paneTitle]) {
+          items[paneTitle] = { total: 0 };
+        }
+        if (!prices[paneTitle][productTitle]) {
+          prices[paneTitle][productTitle] = 0;
+        }
+        if (!items[paneTitle][productTitle]) {
+          items[paneTitle][productTitle] = 0;
+        }
+
+        items[paneTitle].name = [ paneTitleOrig, categoryTitle ].join(': ');
+        prices[paneTitle].total += val * price;
+        items[paneTitle].total += val;
+        prices[paneTitle][productTitle] += val * price;
+        items[paneTitle][productTitle] += val;
+        totalPrice += val * price;
+      });
+
+      if (!$tableLastRow) {
+        $tableLastRow = $('.assortiment-total-price table tbody tr');
+        $table = $('.assortiment-total-price table tbody');
+      }
+
+      $tableLastRow.detach();
+      $table.empty();
+
+      var $totalPrice = $('.assortiment-total-price .total-wrapper span');
+      var full = false;
+      var total = 0;
+      var selectedItems = [];
+      var type = $('input[name="product-option"]:checked').val() || '';
+
+      for (var i in prices) {
+        if (prices.hasOwnProperty(i) && prices[i].total) {
+          full = true;
+          var $tr = $('<tr>')
+            .append(
+              $('<td>').text(i),
+              $('<td>').text(items[i].total + 'x'),
+              $('<td>').text(Math.round(prices[i].total * 100) / 100)
+            );
+
+          $table.append($tr);
+
+          if (items[i].total) {
+            total += items[i].total;
+            for (var j in items[i]) {
+              if (items[i][j] && ['total', 'name'].indexOf(j) === -1) {
+                selectedItems.push(items[i].name + ': ' + j + ': ' + items[i][j] + 'x ' + type);
+              }
+            }
+          }
+        }
+      }
+
+      if (total > 3) {
+        $table.append($tableLastRow);
+      }
+
+      localStorage.setItem('items', JSON.stringify(selectedItems));
+
+      console.log(selectedItems);
+
+      if (full) {
+        $btnActive.addClass('active');
+        $res.show();
+        $emptyRes.hide();
+      }
+      else {
+        $btnActive.removeClass('active');
+        $res.hide();
+        $emptyRes.show();
+      }
+
+      $totalPrice.text(Math.round(totalPrice * 100) / 100);
+      //console.log('totalPrice:', totalPrice);
+      //console.log('prices:', prices);
+      //console.log('items:', items);
+    }
+
   });
-
-
-
 })(jQuery);
